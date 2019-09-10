21a5a413b5cdf44e45859826eb604e40a4a69eed
Anthony Muisyo <muisyoanthony@gmail.com>
2019-09-10 22:30:42 +0300

    Client folder added

--- server/app.js
+++ server/app.js
@@ -29,7 +29,8 @@ app.use(errorHandler);
 //Router
 const apiRouter = require('./router/apiRouter');
 app.use(favicon(__dirname + '/build/favicon.ico'));
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));//Front end
+//app.use(express.static(path.join(__dirname, '../documents'))); //Client documents
 
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
