24434e7139e93f846432920d3af0363324d55e37
Anthony Muisyo <muisyoanthony@gmail.com>
2019-11-24 22:21:53 +0300

    api check temporary disable

--- server/router/apiRouter.js
+++ server/router/apiRouter.js
@@ -7,25 +7,25 @@ const userRouter = require('./user/userRouter');
 const adminRouter = require('./admin/adminRouter');
 
 
-router.use((req, res, next) => {
-    let token = req.headers['authorization_token'];
+// router.use((req, res, next) => {
+//     let token = req.headers['authorization_token'];
 
-    if (token) {
-        jwt.verify(token, global.gConfig.secret , (err, decoded) => {
-            if (err) {
-                return res.json({ message: 'invalid token' });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
+//     if (token) {
+//         jwt.verify(token, global.gConfig.secret , (err, decoded) => {
+//             if (err) {
+//                 return res.json({ message: 'invalid token' });
+//             } else {
+//                 // if everything is good, save to request for use in other routes
+//                 req.decoded = decoded;
+//                 next();
+//             }
+//         });
 
-    } else
-        res.status(403).send({
-            message: 'Unauthorized access.'
-        });
-});
+//     } else
+//         res.status(403).send({
+//             message: 'Unauthorized access.'
+//         });
+// });
 
 router.use('/users', userRouter);
 router.use('/admin', adminRouter);
